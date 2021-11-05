5c4e9b9f2fb6e28e20d127b2ace180e16e92d57c
iluxandro01 <iluxandro@gmail.com>
2021-11-06 00:44:24 +0300

    add save area

---
+++ Assets/Scripts/OK/Extension/SaveAreaFilter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+
+public class SaveAreaFilter : MonoBehaviour
+{
+    private void Awake()
+    {
+        var rectTransform = GetComponent<RectTransform>();
+
+        var safeArea = Screen.safeArea;
+
+        var anchorMin = safeArea.position;
+        var anchorMax = anchorMin + safeArea.size;
+
+        anchorMin.x /= Screen.width;
+        anchorMin.y /= Screen.height;
+
+        anchorMax.x /= Screen.width;
+        anchorMax.y /= Screen.height;
+
+        rectTransform.anchorMin = anchorMin;
+        rectTransform.anchorMax = anchorMax;
+    }
+}
--- Assets/Scripts/OK/Spawner/IncreaseSpeed/SpeedIncreasePerTimeCurve.cs
+++ Assets/Scripts/OK/Spawner/IncreaseSpeed/SpeedIncreasePerTimeCurve.cs
@@ -24,4 +24,6 @@ public class SpeedIncreasePerTimeCurve : ASpeedIncrease
             _currentKey = 0;
         }
     }
+
+   
 }
